d6205a84c2a797a94babc508043f906b3047139f
kookster <kookster@gmail.com>
2007-01-18 06:58:12 +0000

    Adding poller based on daemon

--- generators/processor/processor_generator.rb
+++ generators/processor/processor_generator.rb
@@ -3,9 +3,10 @@ class ProcessorGenerator < Rails::Generator::NamedBase
     record do |m|
       path = 'app/processors'
       m.directory path
-      m.template 'application.rb', File.join(path, "application.rb")
       m.template 'processor.rb', File.join(path, "#{file_name}_processor.rb")
       m.template 'messaging.rb', File.join('config', "messaging.rb")
+      m.file 'application.rb', File.join(path, "application.rb")
+      m.file 'poller', File.join('script', "poller"), { :chmod => 0755 }
     end
   end
 end
