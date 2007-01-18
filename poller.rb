067e3afb36ec956cb19edd54595451114d4690b1
kookster <kookster@gmail.com>
2007-01-18 06:38:07 +0000

    fix the poller script, add in call to load the messaging.rb, fixes issue 4 more

--- poller.rb
+++ poller.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/env ruby
+#make sure stdout and stderr write out without delay for using with daemon like scripts
+STDOUT.sync = true; STDOUT.flush
+STDERR.sync = true; STDERR.flush
+
+#Load the environment and the plugin init
 RAILS_ROOT=File.expand_path(File.join(File.dirname(__FILE__), '..','..','..'))
 load File.join(RAILS_ROOT, 'config', 'environment.rb')
-
-#make sure stdout and stderr write out without delay for using with daemon like scripts
-STDOUT.sync = true
-STDOUT.flush
-
-STDERR.sync = true
-STDERR.flush
+load File.join(RAILS_ROOT, 'config', 'messaging.rb')
 
 #Load the parent processor.rb, then all child processor classes
 puts "Loading #{RAILS_ROOT + '/app/processors/application.rb'}"; load RAILS_ROOT + '/app/processors/application.rb'
 Dir[RAILS_ROOT + '/app/processors/*_processor.rb'].each{|f| puts "Loading #{f}"; load f}
 
-#see if there are any subscriptions
+#See if there are any subscriptions
 if ActiveMessaging::Gateway.subscriptions.empty?
   puts "No subscriptions."
   puts "If you have no processor classes in app/processors, add them using the command:"
