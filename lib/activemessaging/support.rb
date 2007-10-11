93a5a7e23fe0f91103cc34dfcd0e30a9413168c4
kookster <kookster@gmail.com>
2007-10-11 19:44:13 +0000

    fix error with poller and latest edge rails

--- lib/activemessaging/support.rb
+++ lib/activemessaging/support.rb
@@ -1,25 +1,20 @@
 require 'dispatcher' unless defined?(::Dispatcher)
-::Dispatcher.class_eval do
+ActionController::Dispatcher.class_eval do
 
   def self.prepare_application_for_dispatch
-    prepare_application
+    if (self.singleton_methods.include? "prepare_application")
+      prepare_application
+    else
+      new(STDOUT).prepare_application
+    end
   end  
 
   def self.reset_application_after_dispatch
-    reset_after_dispatch
+    if (self.singleton_methods.include? "reset_after_dispatch")
+      reset_after_dispatch
+    else
+      new(STDOUT).cleanup_application
+    end
   end
   
-  # unless ::Dispatcher.respond_to?(:to_prepare)
-  #   
-  #   def prepare_application
-  #     ActiveMessaging.reload_activemessaging
-  #     ActionController::Routing::Routes.reload if Dependencies.load?
-  #     prepare_breakpoint
-  #     require_dependency('application.rb') unless Object.const_defined?(:ApplicationController)
-  #     ActiveRecord::Base.verify_active_connections!
-  #   end
-  # 
-  # end
-  
-
 end
