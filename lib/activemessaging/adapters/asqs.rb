89d6f8ca0810367371ef9eb2c5c912ea5a439e2e
kookster <kookster@gmail.com>
2008-03-05 08:15:46 +0000

    fix asqs adapter for 2008-01-01 version of amazon sqs

--- lib/activemessaging/adapters/asqs.rb
+++ lib/activemessaging/adapters/asqs.rb
@@ -1,3 +1,10 @@
+require 'rubygems'
+require 'net/http'
+require 'openssl'
+require 'base64'
+require 'cgi'
+require 'time'
+
 module ActiveMessaging
   module Adapters
     module AmazonSQS
@@ -7,10 +14,12 @@ module ActiveMessaging
 
         register :asqs
 
-        QUEUE_NAME = 1..80
+        QUEUE_NAME_LENGTH = 1..80
         MESSAGE_SIZE = 1..(256 * 1024)
         VISIBILITY_TIMEOUT = 0..(24 * 60 * 60)
         NUMBER_OF_MESSAGES = 1..255
+        GET_QUEUE_ATTRIBUTES = ['All', 'ApproximateNumberOfMessages', 'VisibilityTimeout']
+        SET_QUEUE_ATTRIBUTES = ['VisibilityTimeout']
 
         #configurable params
         attr_accessor :reliable, :reconnectDelay, :access_key_id, :secret_access_key, :aws_version, :content_type, :host, :port, :poll_interval, :cache_queue_list
@@ -22,12 +31,16 @@ module ActiveMessaging
 
             @access_key_id=cfg[:access_key_id]
             @secret_access_key=cfg[:secret_access_key]
-            @aws_version = cfg[:aws_version]            || '2006-04-01'
-            @content_type = cfg[:content_type]          || 'text/plain'
-            @host = cfg[:host]                          || 'queue.amazonaws.com'
-            @port = cfg[:port]                          || 80
-            @poll_interval = cfg[:poll_interval]        || 1
-            @reconnectDelay = cfg[:reconnectDelay]      || 5
+            @request_expires = cfg[:requestExpires]         || 10
+            @request_retry_count = cfg[:requestRetryCount]  || 5
+            @aws_version = cfg[:aws_version]                || '2008-01-01'
+            @content_type = cfg[:content_type]              || 'text/plain'
+            @host = cfg[:host]                              || 'queue.amazonaws.com'
+            @port = cfg[:port]                              || 80
+            @protocol = cfg[:protocol]                      || 'http'
+            @poll_interval = cfg[:poll_interval]            || 1
+            @reconnect_delay = cfg[:reconnectDelay]         || 5
+            @aws_url="#{@protocol}://#{@host}"
 
             @cache_queue_list = cfg[:cache_queue_list].nil? ? true : cfg[:cache_queue_list]
             @reliable =         cfg[:reliable].nil?         ? true : cfg[:reliable]
@@ -57,8 +70,10 @@ module ActiveMessaging
         # queue_name string, headers hash
         # for sqs, attempt delete the queues, won't work if not empty, that's ok
         def unsubscribe queue_name, message_headers={}
-          @subscriptions[queue_name] -= 1
-          @subscriptions.delete(queue_name) if @subscriptions[queue_name] <= 0
+          if @subscriptions[queue_name]
+            @subscriptions[queue_name] -= 1
+            @subscriptions.delete(queue_name) if @subscriptions[queue_name] <= 0
+          end
         end
 
         # queue_name string, body string, headers hash
@@ -95,98 +110,119 @@ module ActiveMessaging
         
         protected
         
-        #belows are the methods from the REST API
-        def create_queue queue_name
-          validate_new_queue queue_name
-          response = transmit 'POST', "/?QueueName=#{queue_name}"
+        def create_queue(name)
+          validate_new_queue name
+      		response = make_request('CreateQueue', nil, {'QueueName'=>name})
           add_queue response.get_text("//QueueUrl") unless response.nil?
+        end      	
+
+        def delete_queue queue
+          validate_queue queue
+          response = make_request('DeleteQueue', "#{queue.queue_url}")
         end
 
-        def list_queues queue_name_prefix=nil
+        def list_queues(queue_name_prefix=nil)
           validate_queue_name queue_name_prefix unless queue_name_prefix.nil?
-          response = transmit 'GET', queue_name_prefix.nil? ? '/' : "/?QueueNamePrefix=#{queue_name_prefix}"
+          params = queue_name_prefix.nil? ? {} : {"QueueNamePrefix"=>queue_name_prefix}
+      		response = make_request('ListQueues', nil, params)
           response.nil? ? [] : response.nodes("//QueueUrl").collect{ |n| add_queue(n.text) }
         end
         
+        def get_queue_attributes(queue, attribute='All')
+          validate_get_queue_attribute(attribute)
+          params = {'AttributeName'=>attribute}
+          response = make_request('GetQueueAttributes', "#{queue.queue_url}")
+          attributes = {}
+          response.each_node('/GetQueueAttributesResponse/GetQueueAttributesResult/Attribute') { |n|
+            n = n.elements['Name'].text
+            v = n.elements['Value'].text
+            attributes[n] = v
+          }
+          if attribute != 'All'
+            attributes[attribute]
+          else
+            attributes
+          end
+        end
+
+        def set_queue_attribute(queue, attribute, value)
+          validate_set_queue_attribute(attribute)
+          params = {'Attribute.Name'=>attribute, 'Attribute.Value'=>value.to_s}
+          response = make_request('SetQueueAttributes', "#{queue.queue_url}", params)
+        end
+
         def delete_queue queue
           validate_queue queue
-          response = transmit 'DELETE', "#{queue.queue_url}", queue.domain
+          response = make_request('DeleteQueue', "#{queue.queue_url}")
         end
-        
+
+        # in progress
         def send_messsage queue, message
           validate_queue queue
           validate_message message
-          response = transmit 'PUT', "#{queue.queue_url}/back", queue.domain, message
-        end
-        
-        def set_visibility_timeout queue, timeout
-          validate_queue queue
-          validate_timeout timeout
-          response = transmit 'PUT', "#{queue.queue_url}?VisibilityTimeout=#{timeout}", queue.domain
+          response = make_request('SendMessage', queue.queue_url, {'MessageBody'=>message})
+          response.get_text("//MessageId") unless response.nil?
         end
 
         def retrieve_messsages queue, num_messages=1, timeout=nil
           validate_queue queue
           validate_number_of_messages num_messages
           validate_timeout timeout if timeout
-          timeout_path = timeout ? "VisibilityTimeout=#{timeout}&" : ''
-          response = transmit 'GET', "#{queue.queue_url}/front?#{timeout_path}NumberOfMessages=#{num_messages}", queue.domain
+
+          params = {'MaxNumberOfMessages'=>num_messages.to_s}
+          params['VisibilityTimeout'] = timeout.to_s if timeout
+
+          response = make_request('ReceiveMessage', "#{queue.queue_url}", params)
           response.nodes("//Message").collect{ |n| Message.from_element n, response, queue } unless response.nil?
         end
         
-        def get_visibility_timeout queue
-          validate_queue queue
-          response = transmit 'GET', "#{queue.queue_url}/", queue.domain
-          response.get_text('//VisibilityTimeout').to_i
-        end
-        
         def delete_message message
-          delete_message_by_id message.queue, message.id
+          response = make_request('DeleteMessage', "#{message.queue.queue_url}", {'ReceiptHandle'=>message.receipt_handle})
         end
 
-        def delete_message_by_id queue, message_id
-          response = transmit 'DELETE', "#{queue.queue_url}/#{message_id}", queue.domain
-        end
+      	def make_request(action, url=nil, params = {})
+      	  
+      	  url ||= @aws_url
+      	  
+      		# Add Actions
+      		params['Action'] = action
+      		params['Version'] = @aws_version
+      		params['AWSAccessKeyId'] = @access_key_id
+      		params['Expires']= (Time.now + @request_expires).gmtime.iso8601
+      		params['SignatureVersion'] = '1'
 
-        def peek_message queue, message_id
-          response = transmit 'GET', "#{queue.queue_url}/#{message_id}", queue.domain
-          Message.from_element( response.node('//Message'), response, queue)
-        end
-        
-        private
-        
-        def queues
-          return @queues if (@queues && cache_queue_list)
-          @queues = {}
-          list_queues.each{|q| @queues[q.name]=q }
-          return @queues
-        end
+      		# Sign the string
+      		sorted_params = params.sort_by { |key,value| key.downcase }
+      		joined_params = sorted_params.collect { |key, value| key.to_s + value.to_s }
+      		string_to_sign = joined_params.to_s
+      		digest = OpenSSL::Digest::Digest.new('sha1')
+          hmac = OpenSSL::HMAC.digest(digest, @secret_access_key, string_to_sign)
+          params['Signature'] = Base64.encode64(hmac).chomp
 
-        def get_or_create_queue queue_name
-          qs = queues
-          qs.has_key?(queue_name) ? qs[queue_name] : create_queue(queue_name)
-        end
-        
-        def add_queue(url)
-          q = Queue.from_url url
-          queues[q.name] = q if cache_queue_list
-          return q
-        end
+          # Construct request
+          query_params = params.collect { |key, value| key + "=" + CGI.escape(value.to_s) }.join("&")
 
-        # method to do the actual send, generic to get, post, delete, etc.
-        # action - possible values: get, post, delete
-        def transmit(command, url, h=host, body=nil, headers={}, p=port)
-          request_headers = create_headers(command, url, headers, body)
-          request = http_request_factory(command, url, request_headers, body)
-          tryit = true
+          # Put these together to get the request query string
+          request_url = "#{url}?#{query_params}"
+          # puts "request_url = #{request_url}"
+          request = Net::HTTP::Get.new(request_url)
+
+          # You should always retry a 5xx error, as some of these are expected
+    	    tryit = true
+          retry_count = 0
           while tryit
             begin
-              response = SQSResponse.new(http_request(h,p,request))
-              tryit = false unless response.nil?
+              response = SQSResponse.new(http_request(host,port,request))
+              if (response.http_response == Net::HTTPServerError || response.nil?) && retry_count <= @request_retry_count
+          		  retry_count ++
+          		  sleep(@reconnect_delay)
+        		  else
+                tryit = false
+      		    end
             rescue StandardError, TimeoutError
               raise $! unless reliable
-              puts "transmit failed, will retry in #{@reconnectDelay} seconds"
-              sleep @reconnectDelay
+        		  retry_count ++
+        		  sleep(@reconnect_delay)
             end
           end
           check_errors(response)
@@ -196,53 +232,35 @@ module ActiveMessaging
           return Net::HTTP.start(h, p){ |http| http.request(r) }
         end
 
-        def create_headers(cmd, url, headers, body)
-          # set then merge the headers
-          hdrs = { 'AWS-Version'=>@aws_version, 
-                   'Date'=>Time.now.httpdate, 
-                   'Content-type'=>@content_type }
-          hdrs['Content-Length'] = body.length.to_s if (body && (cmd=='POST' or cmd=='PUT'))
-          
-
-          #merge with the passed in headers to allow overrides
-          hdrs.merge! headers
-          
-          # calculate authorization based on set headers
-          hdrs['Authorization'] = create_authorization_signature(cmd, url, hdrs)
-          return hdrs
-        end
-
-        def create_authorization_signature(cmd, url, hdrs)
-          base_url = url.index('?') ? url[0..(url.index('?')-1)] : url
-          to_sign = "#{cmd}\n\n#{hdrs['Content-type']}\n#{hdrs['Date']}\n#{base_url}"
-          # puts to_sign
-          signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret_access_key, to_sign)).strip
-          return "AWS #{access_key_id}:#{signature}"
-        end
-
-        def http_request_factory(cmd, url, headers, body)
-          case cmd
-          when 'GET' then Net::HTTP::Get.new(url,headers)
-          when 'DELETE' then Net::HTTP::Delete.new(url,headers)
-          when 'POST'
-              req = Net::HTTP::Post.new(url,headers)
-              req.body=body
-              req
-          when 'PUT'
-              req = Net::HTTP::Put.new(url,headers)
-              req.body=body
-              req
-          else raise 'Unsupported http request type'
-          end
-        end
-
         def check_errors(response)
           raise response.errors if (response && response.errors?)
           response
         end
         
+        private
+        
+        # internal data structure methods
+        def add_queue(url)
+          q = Queue.from_url url
+          queues[q.name] = q if self.cache_queue_list
+          return q
+        end
+
+        def get_or_create_queue queue_name
+          qs = queues
+          qs.has_key?(queue_name) ? qs[queue_name] : create_queue(queue_name)
+        end
+
+        def queues
+          return @queues if (@queues && cache_queue_list)
+          @queues = {}
+          list_queues.each{|q| @queues[q.name]=q }
+          return @queues
+        end
+
+        # validation methods
         def validate_queue_name qn
-          raise "Queue name, '#{qn}', must be between #{QUEUE_NAME.min} and #{QUEUE_NAME.max} characters." unless QUEUE_NAME.include?(qn.length)
+          raise "Queue name, '#{qn}', must be between #{QUEUE_NAME_LENGTH.min} and #{QUEUE_NAME_LENGTH.max} characters." unless QUEUE_NAME_LENGTH.include?(qn.length)
           raise "Queue name, '#{qn}', must be alphanumeric only." if (qn =~ /[^\w\-\_]/ )
         end
 
@@ -264,6 +282,14 @@ module ActiveMessaging
           raise "Timeout, #{to}, must be between #{VISIBILITY_TIMEOUT.min} and #{VISIBILITY_TIMEOUT.max}." unless VISIBILITY_TIMEOUT.include?(to)
         end
 
+        def validate_get_queue_attribute qa
+          raise "Queue Attribute name, #{qa}, not in list of valid attributes to get: #{GET_QUEUE_ATTRIBUTES.to_sentence}." unless GET_QUEUE_ATTRIBUTES.include?(qa)
+        end
+
+        def validate_set_queue_attribute qa
+          raise "Queue Attribute name, #{qa}, not in list of valid attributes to set: #{SET_QUEUE_ATTRIBUTES.to_sentence}." unless SET_QUEUE_ATTRIBUTES.include?(qa)
+        end
+
         def validate_number_of_messages nom
           raise "Number of messages, #{nom}, must be between #{NUMBER_OF_MESSAGES.min} and #{NUMBER_OF_MESSAGES.max}." unless NUMBER_OF_MESSAGES.include?(nom)
         end
@@ -274,9 +300,10 @@ module ActiveMessaging
         attr_accessor :headers, :doc, :http_response
         
         def initialize response
+          # puts "response.body = #{response.body}"
           @http_response = response
           @headers = response.to_hash()
-          @doc = REXML::Document.new(response.body) if response.kind_of?(Net::HTTPSuccess)
+          @doc = REXML::Document.new(response.body)
         end
       
         def message_type
@@ -284,22 +311,24 @@ module ActiveMessaging
         end
 
         def errors?
-          (not http_response.kind_of?(Net::HTTPSuccess)) or (message_type == "Response")
+          (not http_response.kind_of?(Net::HTTPSuccess)) or (message_type == "ErrorResponse")
         end
 
         def errors
           msg = ""
           if http_response.kind_of?(Net::HTTPSuccess)
             msg = "Errors: "
-            each_node('/Response/Errors/Error') { |n|
-              c = n.elements['Code'].text
-              m = n.elements['Message'].text
-              msg << ", " if msg != "Errors: "
-              msg << "#{c} : #{m}"
-            }
           else
             msg = "HTTP Error: #{http_response.code} : #{http_response.message}"
           end
+
+          each_node('//Error') { |n|
+            c = n.elements['Code'].text
+            m = n.elements['Message'].text
+            msg << ", " if msg != "Errors: "
+            msg << "#{c} : #{m}"
+          }
+
           return msg
         end
       
@@ -321,15 +350,15 @@ module ActiveMessaging
         attr_accessor :name, :pathinfo, :domain, :visibility_timeout
 
         def self.from_url url
-          return Queue.new($3,$2,$1) if url =~ /^http:\/\/(.+)\/(.+)\/([\w-]+)$/
+          return Queue.new($2,$1) if (url =~ /^http:\/\/(.+)\/([-a-zA-Z0-9_]+)$/)
           raise "Bad Queue URL: #{url}"
         end
 
         def queue_url
-          "/#{pathinfo}/#{name}"
+          "#{pathinfo}/#{name}"
         end
 
-        def initialize name, pathinfo, domain, vt=nil
+        def initialize name, domain, vt=nil
           @name, @pathinfo, @domain, @visibility_timeout = name, pathinfo, domain, vt
         end
 
@@ -340,14 +369,14 @@ module ActiveMessaging
 
       # based on stomp message, has pointer to the SQSResponseObject
       class Message
-        attr_accessor :headers, :id, :body, :command, :response, :queue
+        attr_accessor :headers, :id, :body, :command, :response, :queue, :md5_of_body, :receipt_handle
         
         def self.from_element e, response, queue
-          Message.new(response.headers, e.elements['MessageId'].text, e.elements['MessageBody'].text, response, queue)
+          Message.new(response.headers, e.elements['MessageId'].text, e.elements['Body'].text, e.elements['MD5OfBody'].text, e.elements['ReceiptHandle'].text, response, queue)
         end
       
-        def initialize headers, id, body, response, queue, command='MESSAGE'
-          @headers, @id, @body, @response, @queue, @command =  headers, id, body, response, queue, command
+        def initialize headers, id, body, md5_of_body, receipt_handle, response, queue, command='MESSAGE'
+          @headers, @id, @body, @md5_of_body, @receipt_handle, @response, @queue, @command =  headers, id, body, md5_of_body, receipt_handle, response, queue, command
           headers['destination'] = queue.name
         end
       
