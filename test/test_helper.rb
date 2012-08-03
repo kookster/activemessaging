70a12f33680ed458b8dad0d004eb3d52c5f9be20
Lance Cooper <lance.m.cooper@gmail.com>
2012-08-03 10:16:28 -0500

    Make the test suite run.
    Corrects the reset method added by test helper to clear the correct variables.
    
    Adds stub app_root and app_environment as we don't load the ActiveMessaging module.
    
    Fix stomp tests by making them header ordering agnostic.
    
    Added gitignore for the directory created by reliable-msg tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,19 +3,31 @@ rails_environtment_file = File.expand_path(File.dirname(__FILE__) + "/../../../.
 if File.exists? rails_environtment_file
   require rails_environment_file
   APP_ROOT = RAILS_ROOT
+  APP_ENV = Rails.env
 else
   ENV['APP_ENV'] = 'test'
   APP_ENV = 'test'
+  APP_ROOT = File.dirname(__FILE__) + '/app'
   
   $: << File.expand_path(File.dirname(__FILE__) + '/../lib')
   require 'rubygems'
   require 'active_support'
+
+  module ActiveMessaging
+    def self.app_root
+      APP_ROOT
+    end
+
+    def self.app_env
+      APP_ENV
+    end
+  end
+
   require 'activemessaging/message_sender'
   require 'activemessaging/processor'
   require 'activemessaging/gateway'
   require 'activemessaging/filter'
   require 'activemessaging/adapters/test'
-  APP_ROOT = File.dirname(__FILE__) + '/app'
 end
   
 # load other libraries
