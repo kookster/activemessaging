70340ab051f727ac230c324d40028a4e89c15bd6
Marlin W. Pierce <marlpier@cisco.com>
2017-05-17 15:42:48 -0400

    Call to_prepare from config object.

--- lib/activemessaging/railtie.rb
+++ lib/activemessaging/railtie.rb
@@ -4,23 +4,22 @@ require 'activemessaging'
 
 module ActiveMessaging
   class Railtie < Rails::Railtie
-    
     initializer 'activemessaging.initialize' do
       
       ActiveMessaging.load_activemessaging
-      
+
+      # Add a to_prepare block which is executed once in production
+      # and before each request in development
       if defined? Rails
         ActiveMessaging.logger.info "ActiveMessaging: Rails available: Adding dispatcher prepare callback."
-        ActionDispatch::Callbacks.to_prepare do
+        config.to_prepare do
           ActiveMessaging.reload_activemessaging
         end
       end
-
     end
 
     rake_tasks do
       load "tasks/start_consumers.rake"
     end
-    
   end
 end
